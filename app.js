58cf78f83b661d027fdeff407d8f1abbab04049f
bernadetarp <bernadetargn@gmail.com>
2024-06-19 21:09:32 +0700

    feat: shuffling cards from server

--- app.js
+++ app.js
@@ -19,10 +19,44 @@ const messages = [
   },
 ];
 
-io.on("connection", (socket) => {
-  // ...
+const rooms = []
 
-  console.log(socket.id, "a user connected");
+io.on("connection", (socket) => {
+  socket.user = {
+    id: socket.id,
+    name: ""
+  }
+
+  socket.on("Set-Nick", nick => {
+    socket.user.name = nick
+  })
+
+  socket.on("Join-Room", room => {
+    socket.join(room)
+  })
+
+  socket.on("Leave-Room", room => {
+    socket.leave(room)
+  })
+
+  socket.on("generate-shuffled-card", () => {
+    const cardImages = [
+      { src: "/img/helmet-1.png", matched: false },
+      { src: "/img/potion-1.png", matched: false },
+      { src: "/img/ring-1.png", matched: false },
+      { src: "/img/scroll-1.png", matched: false },
+      { src: "/img/shield-1.png", matched: false },
+      { src: "/img/sword-1.png", matched: false },
+    ];
+
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    // console.log(shuffledCards);
+
+    io.to("room_1").emit("game-board-created", shuffledCards)
+  })
 
   socket.emit("messages", messages);
 
@@ -35,7 +69,22 @@ io.on("connection", (socket) => {
     // 3. kita kirim messages yang udah diupdate
     io.emit("messages", messages);
   });
+
+  // ------------------------------------------
+
+
+  socket.on("cards:post", (body) => {
+    // 2.5 di masukin doang ke array
+    // messages.push(body);
+    console.log(body.data, "<<<< ini data dari client");
+
+    // 3. kita kirim messages yang udah diupdate
+    io.emit("cards", cards);
+  });
 });
+
+
+
 
 httpServer.listen(3000, () => {
   console.log("Bisaaaa booss");
